5787ea69f7b4fb43d01c846e91307364424cfd98
SidneyGomes878 <sidneyerickson09@gmail.com>
2025-04-17 16:15:34 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let fruta = "banana "
+
+switch (fruta) {
+    case "banana":
+        console.log("É uma banana")
+        break
+        
+    case (Fruta):
+        conseole.log("É uma fruta")
+
+}
