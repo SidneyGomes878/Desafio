d4b6f4697e821a4573a0398da4b9f79d775b6c6e
SidneyGomes878 <sidneyerickson09@gmail.com>
2025-04-17 17:14:12 -0300

    Desafio Pronto

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-let fruta = "pera";
+let nomeHeroi = "Zeldão";
+let nivelHeroi = 7000;
+let nivel = ""; 
 
-switch (fruta) {
-    case "maça":
-        console.log("A fruta é uma maça.");
-        break;
-
-        case "banana":
-        console.log("A fruta é uma banana.");
-        break
-        default:
-            console.log("A fruta é genérica");
-            break;
+if (nivelHeroi <= 1000) {
+    nivel = "Ferro";
+} 
+else if (nivelHeroi <= 2000) {
+    nivel = "Bronze";
+}
+ else if (nivelHeroi <= 3000) {
+    nivel = "Prata";
+} 
+else if (nivelHeroi <= 4000) {
+    nivel = "Ouro";
+}
+ else if (nivelHeroi <= 5000) {
+    nivel = "Platina";
+} 
+else if (nivelHeroi <= 6000) {
+    nivel = "Diamante";
+} 
+else if (nivelHeroi <= 7000) {
+    nivel = "Mítico";
+} 
+else if (nivelHeroi <= 8000) {
+    nivel = "Lendário";
 }
 
+console.log("O herói " + nomeHeroi + " está no nível " + nivel + " com " + nivelHeroi + " pontos de poder.");
+console.log("Parabéns, " + nomeHeroi)
